ebb8e2f645f09dcab6c287afd239a96032014e67
Zobee <zohebboga@hotmail.com>
2021-05-06 13:56:02 +0000

    Re-committing with the correct URL

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const arg = process.argv[2];
 
-request(`https://api.thecaatapi.com/v1/breeds/search?q=${arg}`, (err, res, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${arg}`, (err, res, body) => {
   if (err) throw new Error(err);
   //console.log(res)
   let data = JSON.parse(body);
